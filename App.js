2008d21f0297fbbc4942afc3e10ad545400af7db
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-03-09 13:09:06 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Zahra Olanrewaju </h2>
+        <h4>Week 4 Home Work </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
